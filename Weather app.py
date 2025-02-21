bbfa3786e1574d4583fe02b8c7fb697fd618af2b
johnson2123 <johnsonvasupalli@yahoo.com>
2025-02-21 23:29:16 +0530

    Update Weather app.py

--- Weather app.py
+++ Weather app.py
@@ -81,7 +81,7 @@ class WeatherApp(QWidget):
 
     def get_weather(self):
 
-        api_key = "6976c6703a075e948d2b3607f71ea437"
+        api_key = ""
         city = self.city_input.text()
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 
